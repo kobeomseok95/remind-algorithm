892df42b136e82525ec20f11a6d5a5ab7323a9d3
kobumssh <kobumssh@naver.com>
2021-10-09 16:59:39 +0900

    60057 문자열 압축 풀이

--- python/programmers/level2/60057.py
+++ python/programmers/level2/60057.py
@@ -1,3 +1,29 @@
-def solution(s):
-    answer = 0
+def solution(string):
+    answer = len(string)
+    maxDivide = len(string) // 2
+    for interval in range(1, maxDivide + 1):
+        answer = min(answer, compress(string, interval))
     return answer
+
+
+def compress(string, interval):
+    count = 1
+    compressed = ''
+    for i in range(0, len(string), interval):
+        front = string[i: i+interval]
+        back = string[i+interval: i+(2*interval)]
+
+        if front == back:
+            count += 1
+        else:
+            compressed += (str(count) if count != 1 else '') + front
+            count = 1
+    return len(compressed)
+
+
+if __name__ == "__main__":
+    print(solution("aabbaccc"))
+    print(solution("ababcdcdababcdcd"))
+    print(solution("abcabcdede"))
+    print(solution("abcabcabcabcdededededede"))
+    print(solution("xababcdcdababcdcd"))
