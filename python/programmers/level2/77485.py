3ef0215731937432a8cd8f7aa3156de3af3d94d2
kobumssh <kobumssh@naver.com>
2021-10-21 14:15:25 +0900

    프로그래머스 77485 행렬 테두리 회전하기 풀이

--- python/programmers/level2/77485.py
+++ python/programmers/level2/77485.py
@@ -1,44 +1,31 @@
 def solution(rows, columns, queries):
+    maps = [[(r * columns) + (c + 1) for c in range(columns)] for r in range(rows)]
     answer = []
-    count = 0
-    maps = initMap(rows, columns)
-    while count < len(queries):
-        maps = rotate(maps, queries[count], answer)
-        count += 1
+    for query in queries:
+        query = [q - 1 for q in query]
+        tmp = maps[query[0]][query[1]]
+        minValue = tmp
 
+        for i in range(query[0] + 1, query[2] + 1):
+            maps[i - 1][query[1]] = maps[i][query[1]]
+            minValue = min(maps[i][query[1]], minValue)
+        for i in range(query[1] + 1, query[3] + 1):
+            maps[query[2]][i - 1] = maps[query[2]][i]
+            minValue = min(maps[query[2]][i], minValue)
+        for i in range(query[2] - 1, query[0] - 1, -1):
+            maps[i + 1][query[3]] = maps[i][query[3]]
+            minValue = min(maps[i][query[3]], minValue)
+        for i in range(query[3] - 1, query[1] - 1, -1):
+            maps[query[0]][i + 1] = maps[query[0]][i]
+            minValue = min(maps[query[0]][i], minValue)
+        maps[query[0]][query[1] + 1] = tmp
+
+        answer.append(minValue)
     return answer
 
 
-def initMap(rows, columns):
-    maps = [[(columns * i) + (j + 1) for j in range(columns)] for i in range(rows)]
-    return maps
-
-
-def rotate(maps, query, answer):
-    visit = [[False for j in range(len(maps[0]))] for i in range(len(maps))]
-    y1, x1, y2, x2 = query[0] - 1, query[1] - 1, query[2] - 1, query[3] - 1
-    moveY, moveX = y1, x1
-    temp = maps[moveY][moveX]
-    minNum = 10001
-    dy, dx = [0, 1, 0, -1], [1, 0, -1, 0]
-    d = 0
-    while True:
-        visit[moveY][moveX] = True
-        if (moveY + dy[d] > y2 or moveY + dy[d] < y1) or (moveX + dx[d] > x2 or moveX + dx[d] < x1):
-            d = (d + 1) % 4
-        moveY += dy[d]
-        moveX += dx[d]
-        if visit[moveY][moveX]:
-            break
-        maps[moveY][moveX], temp = temp, maps[moveY][moveX]
-        minNum = min(minNum, maps[moveY][moveX])
-    answer.append(minNum)
-    return maps
-
-
 if __name__ == "__main__":
-    # print(solution(6, 6, [[2,2,5,4],[3,3,6,6],[5,1,6,3]]))
-    # print(solution(3, 3, [[1,1,2,2],[1,2,2,3],[2,1,3,2],[2,2,3,3]]))
-    # print(solution(100, 97, [[1,1,100,97]]))
-
-    print(solution(5, 4, [[3, 1, 4, 4]]))
+    print(solution(6, 6, [[2,2,5,4],[3,3,6,6],[5,1,6,3]]))
+    print(solution(3, 3, [[1,1,2,2],[1,2,2,3],[2,1,3,2],[2,2,3,3]]))
+    print(solution(3, 4, [[1,2,3,4], [1,1,3,4]]))
+    print(solution(100, 97, [[1,1,100,97]]))
