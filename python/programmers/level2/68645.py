430d588d6331b001f64f0a9135b320b4b8863b37
kobeomseok95 <kobumssh@naver.com>
2021-12-19 14:25:52 +0900

    삼각 달팽이 풀이

---
+++ python/programmers/level2/68645.py
@@ -0,0 +1,22 @@
+def solution(n):
+    answer = [[0] * i for i in range(1, n + 1)]
+    num = 1
+    y, x = -1, 0
+    for i in range(n):
+        for j in range(i, n):
+            if i % 3 == 0:
+                y += 1
+            elif i % 3 == 1:
+                x += 1
+            else:
+                y -= 1
+                x -= 1
+            answer[y][x] = num
+            num += 1
+    return sum(answer, [])
+
+
+if __name__ == "__main__":
+    print(solution(4))
+    print(solution(5))
+    print(solution(6))
