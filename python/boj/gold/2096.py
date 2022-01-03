ce7c3b2ca0dbcdd2e9ab398ad788d59be5f995a3
kobeomseok95 <kobumssh@naver.com>
2022-01-03 13:56:40 +0900

    내려가기 풀이

---
+++ python/boj/gold/2096.py
@@ -0,0 +1,29 @@
+import sys
+read = sys.stdin.readline
+
+N = int(read())
+
+max_dp = [0] * 3
+min_dp = [0] * 3
+
+max_tmp = [0] * 3
+min_tmp = [0] * 3
+
+for i in range(N):
+    a, b, c = map(int, read().split())
+    for j in range(3):
+        if j == 0:
+            max_tmp[j] = a + max(max_dp[j], max_dp[j + 1])
+            min_tmp[j] = a + min(min_dp[j], min_dp[j + 1])
+        elif j == 1:
+            max_tmp[j] = b + max(max_dp[j], max_dp[j + 1], max_dp[j - 1])
+            min_tmp[j] = b + min(min_dp[j], min_dp[j + 1], min_dp[j - 1])
+        else:
+            max_tmp[j] = c + max(max_dp[j], max_dp[j - 1])
+            min_tmp[j] = c + min(min_dp[j], min_dp[j - 1])
+
+    for j in range(3):
+        max_dp[j] = max_tmp[j]
+        min_dp[j] = min_tmp[j]
+
+print(max(max_dp), min(min_dp))
