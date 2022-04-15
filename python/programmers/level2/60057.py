cbc15a00dc3894d91f6efac719e2e733a6b5dd18
kobeomseok95 <kobumssh@naver.com>
2022-04-15 23:50:40 +0900

    문자열 압축 풀이

--- python/programmers/level2/60057.py
+++ python/programmers/level2/60057.py
@@ -1,24 +1,47 @@
 def solution(string):
     answer = len(string)
-    maxDivide = len(string) // 2
-    for interval in range(1, maxDivide + 1):
-        answer = min(answer, compress(string, interval))
+    for interval in range(1, len(string) // 2 + 1):
+        compressed_string = ''
+        compressed_length = 0
+        repeat_count = 1
+        for i in range(0, len(string), interval):
+            temp = string[i: i+interval]
+            if compressed_string == temp:
+                repeat_count += 1
+            else:
+                compressed_length += len(temp)
+                if repeat_count > 1:
+                    compressed_length += len("{}".format(repeat_count))
+                repeat_count = 1
+                compressed_string = temp
+        if repeat_count > 1:
+            compressed_length += len("{}".format(repeat_count))
+        answer = min(answer, compressed_length)
     return answer
 
 
-def compress(string, interval):
-    count = 1
-    compressed = ''
-    for i in range(0, len(string), interval):
-        front = string[i: i+interval]
-        back = string[i+interval: i+(2*interval)]
 
-        if front == back:
-            count += 1
-        else:
-            compressed += (str(count) if count != 1 else '') + front
-            count = 1
-    return len(compressed)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
