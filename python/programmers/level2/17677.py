17ddb5e362662d62d5e9225e133643ae58f971c4
kobeomseok95 <kobumssh@naver.com>
2022-04-20 16:02:06 +0900

    뉴스 클러스터링 풀이

--- python/programmers/level2/17677.py
+++ python/programmers/level2/17677.py
@@ -1,56 +1,25 @@
-import math
-
-
 def solution(str1, str2):
-    snippet1 = makeSnippetList(str1)
-    snippet2 = makeSnippetList(str2)
-    return calculateAnswer(snippet1, snippet2)
+    MULTIPLE_NUMBER = 65536
+    str1_set = create_multiple_set(str1)
+    str2_set = create_multiple_set(str2)
+    similarity = calculate_jaccard_similarity(str1_set, str2_set)
+    return int(similarity * MULTIPLE_NUMBER)
 
 
-def makeSnippetList(string):
-    snippetList = list()
-    string = string.lower()
+def create_multiple_set(string):
+    str_set = []
     for i in range(len(string) - 1):
-        snippet = string[i:i+2]
-        if snippet.isalpha():
-            snippetList.append(snippet)
-    return snippetList
+        sliced = string[i: i+2].lower()
+        if sliced.isalpha():
+            str_set.append(sliced)
+    return str_set
 
 
-def calculateAnswer(snippets1, snippets2):
-    set1 = set(snippets1)
-    set2 = set(snippets2)
-    intersection = set1.intersection(set2)
-    union = set1.union(set2)
-    return calculateSimilarity(snippets1, snippets2, intersection, union)
-
-
-def calculateSimilarity(snippets1, snippets2, intersection, union):
-    MULTIPLE = 65536
-
-    if len(union) == 0:
-        return MULTIPLE
-
-    intersectionCount = 0
-    unionCount = 0
-    for i in intersection:
-        intersectionCount += min(snippets1.count(i), snippets2.count(i))
-    for u in union:
-        unionCount += max(snippets1.count(u), snippets2.count(u))
-
-    return math.floor((intersectionCount / unionCount) * MULTIPLE)
-
-
-if __name__ == "__main__":
-    print("===================================")
-    print(solution("ab", "ab"))
-    print("===================================")
-    print(solution("+++", ""))
-    print("===================================")
-    print(solution("FRANCE", "french"))
-    print("===================================")
-    print(solution("handshake", "shake hands"))
-    print("===================================")
-    print(solution("aa1+aa2", "AAAA12"))
-    print("===================================")
-    print(solution("E=M*C^2", "e=m*c^2"))
+def calculate_jaccard_similarity(str1_set, str2_set):
+    intersect = set(str1_set) & set(str2_set)
+    union = set(str1_set) | set(str2_set)
+    if not intersect and not union:
+        return 1
+    intersect_sum = sum([min(str1_set.count(i), str2_set.count(i)) for i in intersect])
+    union_sum = sum([max(str1_set.count(i), str2_set.count(i)) for i in union])
+    return intersect_sum / union_sum
