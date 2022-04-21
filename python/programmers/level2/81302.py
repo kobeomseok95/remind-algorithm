3144d0c8b200bec02b27ab74b809c4a6b5675329
kobeomseok95 <kobumssh@naver.com>
2022-04-21 13:26:56 +0900

    거리두기 확인하기 풀이

--- python/programmers/level2/81302.py
+++ python/programmers/level2/81302.py
@@ -1,75 +1,36 @@
 from collections import deque
 
-SIZE = 5
-move = ((0, 1), (1, 0), (-1, 0), (0, -1))
-
-
-def set_up(place):
-    arr = []
-    seated = []
-    for i, string in enumerate(place):
-        for j, s in enumerate(string):
-            if s == 'P':
-                seated.append((i, j))
-        arr.append(list(string))
-    return arr, seated
-
-
-def is_in(y, x):
-    if -1 < y < SIZE and -1 < x < SIZE:
-        return True
-    return False
-
-
-def bfs(arr, sy, sx):
-    q = deque()
-    q.append((sy, sx))
-
-    table = [[-1 for _ in range(SIZE)] for _ in range(SIZE)]
-    table[sy][sx] = 0
-    while q:
-        y, x = q.popleft()
-        for i in range(4):
-            ny = y + move[i][0]
-            nx = x + move[i][1]
-
-            if not is_in(ny, nx):
-                continue
-            if arr[ny][nx] != 'X' and table[ny][nx] == -1:
-                table[ny][nx] = table[y][x] + 1
-                q.append((ny, nx))
-
-    return table
-
 
 def solution(places):
     answer = []
     for place in places:
-        arr, seated = set_up(place)
-        ok = True
-
-        for sit in seated:
-            table = bfs(arr, sit[0], sit[1])
-            for other_sit in seated:
-                if other_sit != sit:
-                    if -1 < table[other_sit[0]][other_sit[1]] <= 2:
-                        ok = False
-                        break
-            if not ok:
-                break
-        if ok:
-            answer.append(1)
-        else:
-            answer.append(0)
-
+        maps = [list(p) for p in place]
+        answer.append(is_follow_distance_rules(maps))
     return answer
 
 
-if __name__ == "__main__":
-    print(solution([
-        ["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"],
-        ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"],
-        ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"],
-        ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"],
-        ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]
-    ]))
+def is_follow_distance_rules(maps):
+    for i in range(len(maps)):
+        for j in range(len(maps[0])):
+            if maps[i][j] == 'P':
+                if not bfs(maps, i, j):
+                    return 0
+    return 1
+
+
+def bfs(maps, i, j):
+    dy, dx = [-1, 1, 0, 0], [0, 0, -1, 1]
+    q = deque()
+    q.append((0, i, j))
+    visit = set()
+    visit.add((i, j))
+    while q:
+        manhattan_distance, y, x = q.popleft()
+        for d in range(4):
+            ny, nx = y + dy[d], x + dx[d]
+            if 0 <= ny < 5 and 0 <= nx < 5 and (ny, nx) not in visit and manhattan_distance < 2:
+                if maps[ny][nx] == 'O':
+                    q.append((manhattan_distance + 1, ny, nx))
+                elif maps[ny][nx] == 'P':
+                    return False
+    return True
