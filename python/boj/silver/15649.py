bef7fbb5fe95094a61d6e78bcb50b2c7a182c3ad
kobeomseok95 <kobumssh@naver.com>
2022-05-12 15:37:24 +0900

    N과 M(1) 풀이

---
+++ python/boj/silver/15649.py
@@ -0,0 +1,19 @@
+import sys
+read = sys.stdin.readline
+
+
+def dfs():
+    if len(s) == M:
+        print(' '.join(map(str, s)))
+        return
+    for i in range(1, N + 1):
+        if i not in s:
+            s.append(i)
+            dfs()
+            s.pop()
+
+
+if __name__ == "__main__":
+    N, M = map(int, read().split())
+    s = []
+    dfs()
