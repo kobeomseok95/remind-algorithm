aa84a14ac1cfd9823be8e94f38f3da93a3faa207
kobeomseok95 <kobumssh@naver.com>
2022-05-25 16:28:50 +0900

    쿼드압축 후 개수 세기 풀이

--- python/programmers/level2/68936.py
+++ python/programmers/level2/68936.py
@@ -1,28 +1,22 @@
-import sys
-sys.setrecursionlimit(10**6)
-
-
-def compression(y, x, length, arr, answer):
-    start = arr[y][x]
-    for i in range(y, y + length):
-        for j in range(x, x + length):
-            if arr[i][j] != start:
-                length //= 2
-                compression(y, x, length, arr, answer)
-                compression(y, x + length, length, arr, answer)
-                compression(y + length, x, length, arr, answer)
-                compression(y + length, x + length, length, arr, answer)
-                return
-    answer[start] += 1
-
-
 def solution(arr):
     answer = [0, 0]
-    length = len(arr)
-    compression(0, 0, length, arr ,answer)
+    N = len(arr)
+    compress(arr, answer, 0, 0, N)
     return answer
 
 
-if __name__ == "__main__":
-    print(solution([[1,1,0,0],[1,0,0,0],[1,0,0,1],[1,1,1,1]]))
-    print(solution([[1,1,1,1,1,1,1,1],[0,1,1,1,1,1,1,1],[0,0,0,0,1,1,1,1],[0,1,0,0,1,1,1,1],[0,0,0,0,0,0,1,1],[0,0,0,0,0,0,0,1],[0,0,0,0,1,0,0,1],[0,0,0,0,1,1,1,1]]))
+def compress(arr, answer, y, x, N):
+    element = arr[y][x]
+    for i in range(y, y + N):
+        for j in range(x, x + N):
+            if arr[i][j] != element:
+                NN = N // 2
+                compress(arr, answer, y, x, NN)
+                compress(arr, answer, y, x + NN, NN)
+                compress(arr, answer, y + NN, x, NN)
+                compress(arr, answer, y + NN, x + NN, NN)
+                return
+    if element == 0:
+        answer[0] += 1
+    else:
+        answer[1] += 1
