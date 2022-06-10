c237a529d95876e7628ce9ea52c8a4b781080c0b
kobeomseok95 <kobumssh@naver.com>
2022-06-10 15:06:45 +0900

    플로이드 와샬 버전

--- python/programmers/level3/49191.py
+++ python/programmers/level3/49191.py
@@ -1,26 +1,20 @@
-from collections import deque
-
-
 def solution(n, results):
-    winners = [[] for _ in range(n + 1)]
-    losers = [[] for _ in range(n + 1)]
-    for win, lose in results:
-        winners[win].append(lose)
-        losers[lose].append(win)
-
     answer = 0
-    for i in range(1, n + 1):
-        visit = [False] * (n + 1)
-        visit[0] = True
-        visit[i] = True
-        for result_record in [winners, losers]:
-            player_queue = deque()
-            player_queue.append(i)
-            while player_queue:
-                player_no = player_queue.popleft()
-                for target_player_no in result_record[player_no]:
-                    if not visit[target_player_no]:
-                        visit[target_player_no] = True
-                        player_queue.append(target_player_no)
-        answer += 1 if False not in visit else 0
+    board = [[0] * n for _ in range(n)]
+    for a, b in results:
+        board[a - 1][b - 1] = 1
+        board[b - 1][a - 1] = -1
+
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if i == j or board[i][j] in [-1, 1]:
+                    continue
+                if board[i][k] == board[k][j] == 1:
+                    board[i][j] = 1
+                    board[j][i] = board[k][i] = board[j][k] = -1
+    for row in board:
+        if row.count(0) == 1:
+            answer += 1
+
     return answer
